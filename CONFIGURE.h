37a7e426253acfce4ce000b991562da3f4b41dce
codingchris779 <33784082+codingchris779@users.noreply.github.com>
2025-06-15 15:06:43 -0400

    j

--- CONFIGURE.h
+++ CONFIGURE.h
@@ -5,12 +5,12 @@
 
 #include <stdint.h>
 
-bool debug = 0;
+bool debug = 1;
 
 //Safe operating conditions
-const float max_temp = 55;
+const float max_temp = 59;
 const float min_temp = 0;
-const float OV = 4.15;       //over-voltage limit (spelled with an "oh" not zero) (V)
+const float OV = 4.19;       //over-voltage limit (spelled with an "oh" not zero) (V)
 const float UV = 2.5;       //under-voltage limit (V)
 const float max_diff = 1.7;   //max difference between min and max cell for open parallel cell detection
 const int watchdog_timeout = 0;  //watchdog timeout (in seconds). setting to 0 will DISABLE timer. 
@@ -21,7 +21,7 @@ const int num_cells = 17;       //cells per board
 const int num_parrallel = 3;
 
 //BMS operation mode. Leave as empty string to determine mode during runtime
-String mode = "drive";     //"", "charge", "standby", "drive", "debug"
+String mode = "";     //"", "charge", "standby", "drive", "debug"
 
 //CAN Bus Parameters
 uint16_t BMS_ID = 0x123;             //standard ID of BMS TX messages
@@ -29,12 +29,12 @@ uint32_t INV_TX_ID = 0x0A7;         //CAN Message ID of message send from invert
 uint32_t CHG_TX_ID = 0x18FF50E5;    //CAN Message ID of messages sent from charger
 
 //charging parameters
-uint16_t CHG_voltage = 356;
-uint16_t CHG_current = 6;
+uint16_t CHG_voltage = 357;
+uint16_t CHG_current = 8;
 float _qt = 12.6 * 60; //total capacity (coulumbs): total capacity (Ah) * 60s/1hr
 
 //balancing parameters
-float balance_threshold = 3.85;    //will not balance cells below this threshold (V)
+float balance_threshold = 4.2;    //will not balance cells below this threshold (V)
 float max_differnce = 0.3;    //will not continue charging if max-min cell exceeds this threshold
 
 //sense board parameters
@@ -52,11 +52,12 @@ const int SD_interval = 10;           //This needs to be the longest interval
 
 //SD Card
 float SD_card_size = 32;   //SD card size in Gb
-int file_read_begin = 222;  //Starting file number of file to dump through serial
+int file_read_begin = 306;  //Starting file number of file to dump through serial
 
 
 //Power Calculations
-const int FULL_POWER_LEVEL_KW = 60; //Peak Power level
-const int FULL_POWER_TEMP_C = 38; // Max temperature that has full power
-const int ZERO_POWER_TEMP_C = 55; // Power linearly decreases from full power to zero power at this temperature
+const int FULL_POWER_LEVEL_KW = 80; //Peak Power level
+const int FULL_ENDURANCE_CURRENT = 40;
+const int FULL_POWER_TEMP_C = 55; // Max temperature that has full power
+const int ZERO_POWER_TEMP_C = 57; // Power linearly decreases from full power to zero power at this temperature
 #endif
